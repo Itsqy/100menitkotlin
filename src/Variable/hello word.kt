e2543989fa3c2661b8feebcffaf8f1abbef73564
Itsqy <syatriarifqi@gmail.com>
2022-02-26 21:34:43 +0700

    push

---
+++ src/Variable/hello word.kt
@@ -0,0 +1,38 @@
+package Variable
+
+fun main() {
+
+
+    /*
+    commntar pada kotlin
+    dfdsf
+    asdfa
+    adfa
+    afds
+    */
+    print(" i love kotlin")
+
+    //     ctrl + /
+
+    /*
+    assalamualaikum
+    selamat datang di arief hidayat
+    tutorial pemrograman
+     */
+
+    /*
+    / + * enter
+     */
+
+    /*
+    * bedanya
+    * setiap bari terdapat bintang di awal kalimat
+    * */
+//    ctrl +/ enter
+
+    /**
+     * ctrl + shift * trus Fungsi.tambah bintang lagi
+     * setelah itu dienter
+     * */
+
+}
